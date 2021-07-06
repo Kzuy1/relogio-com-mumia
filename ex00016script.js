adc1c98737396faa11faf5401dbcba12533788ad
Kzuy1 <85893705+Kzuy1@users.noreply.github.com>
2021-07-05 21:38:19 -0300

    Add files via upload

---
+++ ex00016script.js
@@ -0,0 +1,47 @@
+function verificar() {
+    var data = new Date()
+    var ano =  data.getFullYear()
+    var calano = document.getElementById("txtano")
+    var res = document.getElementById("res")
+    if (calano.value.length == 0 || calano.value > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente.")
+        // Aq em cima ele analisa se o ano colocado no verificar é 0 ou maior que que o ano atual.
+    } else {
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(calano.value)
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+        if (fsex[0].checked) {
+            genero = "Homem"
+            if(idade >= 0 && idade <10){
+                img.setAttribute("src", "foto-bebe-m.png")
+            } else if (idade <30) {
+                img.setAttribute("src", "foto-jovem-m.png")
+            } else if (idade <50) {
+                img.setAttribute("src", "foto-adulto-m.png")
+            } else if (idade <130) {
+                img.setAttribute("src", "foto-idoso-m.png")
+            } else {
+                img.setAttribute("src", "mumia.png")
+            }
+
+        } else if (fsex[1].checked) {
+            genero = "Mulher"
+            if(idade >= 0 && idade <10){
+                img.setAttribute("src", "foto-bebe-f.png")
+            } else if (idade <30) {
+                img.setAttribute("src", "foto-jovem-f.png")
+            } else if (idade <50) {
+                img.setAttribute("src", "foto-adulto-f.png")
+            } else if (idade <130) {
+                img.setAttribute("src", "foto-idoso-f.png")
+            } else {
+                img.setAttribute("src", "mumia.png")
+            }
+        }
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos. <br>`
+        res.appendChild(img)
+    }
+}
